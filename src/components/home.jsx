212edf0928d7b5bfc91ad82e8bbd1bc9b2855c9f
castynet <riungemaina@gmail.com>
2021-02-03 10:03:53 +0300

    components/home: created

---
+++ src/components/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import TopFeatured from "./topFeatured";
+
+export default function () {
+  return (
+    <>
+      <TopFeatured />
+    </>
+  );
+}
