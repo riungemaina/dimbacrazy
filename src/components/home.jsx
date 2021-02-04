1ffd4891a0e426ea422d92bf9af5047fa2d075de
castynet <riungemaina@gmail.com>
2021-02-04 13:35:50 +0300

    components/home: added body posts

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import TopFeatured from "./topFeatured";
+import BodyPosts from "./bodyPosts";
 
 export default function () {
   return (
     <>
       <TopFeatured />
+      <BodyPosts />
     </>
   );
 }
