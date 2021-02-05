d0fc42fd7b2ebd93620d26ac81bf162d05f7168d
castynet <riungemaina@gmail.com>
2021-02-05 10:20:33 +0300

    App: imported footer & single posts

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import styled from "styled-components";
 
 import Nav from "./components/nav";
 import Home from "./components/home";
+import Footer from "./components/footer";
+import SinglePost from "./components/singlePost";
 
 export default function App() {
   return (
     <>
       <Nav />
       <Page>
-        <Home />
+        <SinglePost />
+        {/* <Home /> */}
       </Page>
+      <Footer />
     </>
   );
 }
@@ -19,5 +23,6 @@ const Page = styled.div`
   max-width: 980px;
   background-color: #fff;
   margin: 0 auto;
-  padding: 30px 10px;
+  padding: 30px 10px 0;
+  min-height: 85vh;
 `;
