990cacdaa50eade584599b0eb97f95f781c353b6
castynet <riungemaina@gmail.com>
2021-02-08 16:16:42 +0300

    App: added context & load logic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from "react";
 import styled from "styled-components";
+import { useApi } from "./components/context";
 
 import Nav from "./components/nav";
 import Home from "./components/home";
 import Footer from "./components/footer";
 import SinglePost from "./components/singlePost";
+import Loader from "./components/loader";
 
 export default function App() {
+  const api = useApi();
+  const Loaded = api.Loaded();
+
+  function Content() {
+    if (Loaded) {
+      return <Home />;
+    } else return <Loader />;
+  }
   return (
     <>
       <Nav />
       <Page>
         <SinglePost />
-        {/* <Home /> */}
+        {/* <Content /> */}
       </Page>
       <Footer />
     </>
