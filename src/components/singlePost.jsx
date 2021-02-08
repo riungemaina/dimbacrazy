dd87710645d73e9692bd34342c682a3045f1712f
castynet <riungemaina@gmail.com>
2021-02-08 11:27:46 +0300

    components/singlePost: styled

--- src/components/singlePost.jsx
+++ src/components/singlePost.jsx
@@ -1,39 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import ReactMarkdown from "react-markdown";
+import file from "./md/posts.md";
 
 export default function () {
+  const [markdown, setMarkdown] = useState("");
+
+  useEffect(() => {
+    fetch(file)
+      .then((res) => res.text())
+      .then((text) => setMarkdown(text));
+  }, []);
+
   return (
     <>
-      <h1>SOUTHAMPTON LOSE 9-0 TO A FLYING MANCHESTER UNITED</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      </p>
-      <p>
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt.
-      </p>
-      <p>
-        Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit, sed quia non numquam eius modi tempora
-        incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim
-        ad minima veniam, quis nostrum exercitationem ullam corporis suscipit
-        laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel
-        eum iure reprehenderit qui in ea voluptate velit esse quam nihil
-        molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
-        nulla pariatur?
-      </p>
+      <Post>
+        <ReactMarkdown source={markdown} />
+      </Post>
     </>
   );
 }
+
+const Post = styled.div`
+    padding: 20px ;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-width: 700px;
+    margin: 0 auto;
+    border-left: 1px solid #aaa;
+    border-right: 1px solid #aaa;
+    font-family: 'Titillium Web', sans-serif;
+}
+
+  img {
+    padding: 20px 0;
+    height: 300px;
+    width: auto;
+  }
+
+  p{
+    padding: 5px 0;
+    text-align: justify;
+    font-size: 1.05em;
+  }
+
+  h1, h2, h3, h4, h5, h6{
+    padding: 0 0 20px 0;
+    text-align: center;
+  }
+`;
